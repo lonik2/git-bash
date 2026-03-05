697a8b4afaf229588b8229bf1b661a2375dd6341
mechikuroki <kurokimercedes@gmail.com>
2026-03-05 15:38:41 -0300

    primer commit

---
+++ aaa.py
@@ -0,0 +1,12 @@
+aba = "brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr".isascii()
+
+nep = "漢漢".isascii()
+
+print (aba)
+print (nep)
+
+#este codigo sirve para saber si la palabra/oracion escrita esta todo en los caracteres ASCII.
+
+import this
+
+this
---
+++ listilla.py
@@ -0,0 +1,18 @@
+fallecidos = ["kirk","jeffrey","menchito"]
+
+for x in fallecidos:
+    print (f"hola {x}, veni a cenar")
+
+print ("menchito: no puedo bro, estoy en tamaulipas.")
+
+fallecidos[2] = "maestro splinter"
+
+print (f"oh,que mal.Vos podes {fallecidos[2]}?")
+
+print ("eh chavales, invitare mas gente ya que la mesa es muy grande")
+
+fallecidos.insert(0, "aballay")
+fallecidos.insert(2, "matute")
+fallecidos.append("hola yo soy mateo")
+
+print (f"{fallecidos[0]},{fallecidos[2]},{fallecidos[5]}, vienen chavalines??")
