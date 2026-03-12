cc37cec54c566d7cf467ed58dff9a0eb64b888bc
Lucas <lonyszczyk@alumno.huergo.edu.ar>
2026-03-12 13:29:49 -0300

    ej 13,4.py

---
+++ ej13,4.py
@@ -0,0 +1,13 @@
+edad = 20
+if edad < 2:
+    print ("es un bebe")
+elif edad >= 2 and edad < 4:
+    print ("es un nene chiquito")
+elif edad >=4 and edad < 13:
+    print ("es un nene")
+elif edad >= 13 and edad < 20:
+    print ("es un adolescente")
+elif edad >= 20 and edad < 65:
+    print ("es un adulto")
+else:
+    print ("es un adulto mayor")
